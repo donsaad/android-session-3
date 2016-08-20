4558150ca759523cab59abb3cea0a3dc942867ea
Mohamed Saad <mohamedsaadamin.cis@gmail.com>
2016-08-20 21:58:54 +0200

    Send email intent

--- app/src/main/java/io/donsaad/session3/MainActivity.java
+++ app/src/main/java/io/donsaad/session3/MainActivity.java
@@ -1,48 +1,42 @@
 package io.donsaad.session3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<String> listValues;
+    private EditText to;
+    private EditText subject;
+    private EditText content;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = (ListView) findViewById(R.id.lv_names);
+        to = (EditText) findViewById(R.id.et_to);
+        subject = (EditText) findViewById(R.id.et_subject);
+        content = (EditText) findViewById(R.id.et_content);
 
-        listValues = new ArrayList<>();
-        listValues.add("The Rock");
-        listValues.add("Manchester United");
-        listValues.add("ROONEY");
-        listValues.add("Cantona");
-        listValues.add("#POGBACK");
-        listValues.add("#ZlatanTime");
-        listValues.add("Android");
-        listValues.add("Lollipop");
-        listValues.add("Ice Cream Sandwich");
-        listValues.add("Jelly Bean");
-        listValues.add("Froyo");
+        Button send = (Button) findViewById(R.id.btn_send);
 
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this,
-                R.layout.list_item_name, listValues);
-        listView.setAdapter(myAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        send.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, "You clicked: " + listValues.get(position), Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { to.getText().toString() });
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject.getText().toString());
+                emailIntent.putExtra(Intent.EXTRA_TEXT, content.getText().toString());
+                emailIntent.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(emailIntent, "Choose your favorite app: "));
             }
         });
+
     }
 
 }
